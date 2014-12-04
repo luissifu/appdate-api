b33c620cd6afe86144c11bba6384048b307f099d
luissifu@work <luis.e.sifuentes@gmail.com>
2014-12-04 14:58:32 -0600

    created home controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_filter :authenticate_user!
   
   def index
-    #redirect_to user_path if user_signed_in?
+    redirect_to home_path if user_signed_in?
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  #pages contoller
   root 'pages#index'
+  get 'index' => 'pages#index'
 
+  #home contoller
+  get 'home' => 'home#index'
+
+  #devise (auth)
   devise_for :users
-
 end
